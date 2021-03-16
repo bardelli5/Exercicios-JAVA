ca86a36264f6ef8b70ae066b303971be28033a2c
Lucas Bardelli <lucas.bardelli@hotmail.com>
2021-03-16 19:17:32 -0300

    exercicio (while)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,28 +7,15 @@ public class Main {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        double salario = sc.nextDouble();
+        int x = sc.nextInt();
+        int soma = 0;
 
-        double imposto;
-        if (salario <= 2000.0) {
-            imposto = 0.0;
-        }
-        else if (salario <= 3000.0) {
-            imposto = (salario - 2000.0) * 0.08;
-        }
-        else if (salario <= 4500.0) {
-            imposto = (salario - 3000.0) * 0.18 + 1000.0 * 0.08;
-        }
-        else {
-            imposto = (salario - 4500.0) * 0.28 + 1500.0 * 0.18 + 1000.0 * 0.08;
+        while (x != 0) {
+            soma += x;
+            x = sc.nextInt();
         }
 
-        if (imposto == 0.0) {
-            System.out.println("Isento");
-        }
-        else {
-            System.out.printf("R$ %.2f%n", imposto);
-        }
+        System.out.println("soma = " + soma);
         sc.close();
     }
 }
